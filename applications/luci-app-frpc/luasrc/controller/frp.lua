6a8a4baad4dd858d6dfa9c5c82157d19e76f8f04
fshh1988 <276355108@qq.com>
2022-06-09 19:19:03 +0800

    some fix

--- applications/luci-app-frpc/luasrc/controller/frp.lua
+++ applications/luci-app-frpc/luasrc/controller/frp.lua
@@ -5,9 +5,9 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "frp"}, cbi("frp/basic"), _("Frp Setting"), 100).dependent = true
-	entry({"admin", "services", "frp", "config"}, cbi("frp/config")).leaf = true
-	entry({"admin", "services", "frp", "status"}, call("act_status")).leaf = true
+	entry({"admin", "services", "frpc"}, cbi("frp/basic"), _("Frp Setting"), 100).dependent = true
+	entry({"admin", "services", "frpc", "config"}, cbi("frp/config")).leaf = true
+	entry({"admin", "services", "frpc", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
